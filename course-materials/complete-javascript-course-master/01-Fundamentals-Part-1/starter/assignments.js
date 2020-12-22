7109e112584b0250ae872c77ebd8a5cc83df5eb0
mr-justice <justice.douglas@hey.com>
2020-12-22 09:16:22 -0500

    Compled Lecture: Data Types assignments

--- course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -1,4 +1,5 @@
-/*
+// Lecture: Values and Variables
+
 let country = 'U.S.A'
 let continent = 'North America'
 let population = 375000000
@@ -6,4 +7,12 @@ let population = 375000000
 console.log(country);
 console.log(continent);
 console.log(population);
-*/
+
+// Lecture: Data Types
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof population);
+console.log(typeof country);
--- course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,6 @@
 /*
+// Lecture: Values and Variables
+
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -20,7 +22,26 @@ let person = 'jonas'
 let PI = 3.1415;
 */
 
-true;
-console.log(true);
+/*
+// Lecture: Data Types
+
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
