bea586f5232be0f5d1588dd54fc17bf3e5e0e09c
mr-justice <justice.douglas@hey.com>
2020-12-22 09:50:34 -0500

    Lecture: let, const and var
    Completed lecture and assignment

--- course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,8 @@
+/*
 // Lecture: Values and Variables
 
 let country = 'U.S.A'
-let continent = 'North America'
+const continent = 'North America'
 let population = 375000000
 
 console.log(country);
@@ -9,10 +10,15 @@ console.log(continent);
 console.log(population);
 
 // Lecture: Data Types
-let isIsland = false;
+
+const isIsland = false;
 let language;
 
 console.log(typeof isIsland);
 console.log(typeof language);
 console.log(typeof population);
 console.log(typeof country);
+
+// Lecture: let, const, and var
+language = 'English';
+*/
--- course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -45,3 +45,16 @@ console.log(typeof year);
 
 console.log(typeof null);
 */
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Douglas'
+console.log(lastName);
