48d02e5726d81ac1a757dbd0d0ee93afba5edf92
mr-justice <justice.douglas@hey.com>
2020-12-23 17:55:54 -0500

    Completed Lecture: Operator Precedence

--- course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -91,4 +91,21 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+
+// Lecture: Operator Precedence
+
+const now = 2037;
+const ageJustice = now - 1995;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJustice + ageSarah) / 2;
+console.log(averageAge);
+console.log(ageJustice, ageSarah, averageAge);
+
 */
