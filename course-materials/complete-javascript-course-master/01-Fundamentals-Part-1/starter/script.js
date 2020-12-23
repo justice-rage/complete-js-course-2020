ef53283ec1a63bfff6c4c4c42968d009449a3652
mr-justice <justice.douglas@hey.com>
2020-12-23 08:29:33 -0500

    Completed Basic Operators Lecture

--- course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -44,7 +44,8 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
+
+// Lecture: let, const and var
 
 let age = 30;
 age = 31;
@@ -58,3 +59,34 @@ job = 'teacher';
 
 lastName = 'Douglas'
 console.log(lastName);
+*/
+
+// Math Opertors 
+const now = 2037;
+const ageJustice = now - 1995;
+const ageSarah = now - 2018;
+console.log(ageJustice, ageSarah);
+
+console.log(ageJustice * 2, ageJustice / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Justice';
+const lastName = 'Douglas';
+console.log(firstName + ' ' + lastName);
+
+// Assignment Operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison Operators
+console.log(ageJustice > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
