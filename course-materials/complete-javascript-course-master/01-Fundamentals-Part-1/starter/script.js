606961f30708dd6535ea9f4b6b8051dbb0d05139
mr-justice <justice.douglas@hey.com>
2020-12-24 15:04:23 -0500

    Completed Lecture
    Lecture: Truthy and Falsy Values

--- course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -205,5 +205,28 @@ console.log(`23` > `18`)
 let n = `1` + 1; // `11`
 n = n - 1;
 console.log(n);
-
 */
+
+// Lecture: Truthy and Falsy Values
+
+// 5 falsy values: 0, '', undefined, null, Nan
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 0;
+if (money) {
+    console.log(`Don't spend it all.`)
+} else {
+    console.log(`You should get a job!`)
+}
+
+let height = 0;
+if (height) {
+    console.log(`Height is defined.`)
+} else {
+    console.log(`Height is undefined.`)
+}
