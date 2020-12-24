b8f9975c000e85d82f07e86fd8391d1cc705f164
mr-justice <justice.douglas@hey.com>
2020-12-24 15:26:57 -0500

    Completed Lecture
    Equality Operators: == vs. ===

--- course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -205,7 +205,6 @@ console.log(`23` > `18`)
 let n = `1` + 1; // `11`
 n = n - 1;
 console.log(n);
-*/
 
 // Lecture: Truthy and Falsy Values
 
@@ -230,3 +229,26 @@ if (height) {
 } else {
     console.log(`Height is undefined.`)
 }
+
+// Lecture: Equality Operators: == vs. ===
+
+const age = `18`;
+if (age === 18) console.log(`You just became an adult. (strict)`);
+
+if (age == 18) console.log(`You just became an adult. (loose)`);
+
+const favorite = Number(prompt(`What's your favorite number?`));
+
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite == 23) {
+    console.log(`Cool! 23 is an amazing number!`)
+} else if (favorite === 7) {
+    console.log(`7 is also a cool number!`)
+} else if (favorite === 9) {
+    console.log(`9 is a cool number.`)
+} else {
+    console.log(`Number is not 23, 7 or 9.`)
+} if (favorite !== 23) console.log(`Why not 23?`);
+*/
