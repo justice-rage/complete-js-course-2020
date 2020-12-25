4f95761e1932ad4be67604e398a2a5179af4530a
mr-justice <justice.douglas@hey.com>
2020-12-25 13:39:18 -0500

    Completed Lecture
    The Switch Statement

--- course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ course-materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -279,7 +279,6 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
     console.log(`Someone else should drive.`);
 }
-*/
 
 // Coding Challenge #3
 
@@ -322,3 +321,46 @@ if ((dolphinsAverageScore && koalasAverageScore) < 100) {
 } else if (koalasAverageScore === dolphinsAverageScore) {
     console.log(`It's a draw!`)
 }
+
+// Lecture: The Switch statement
+
+const day = 'monday';
+
+// switch (day) {
+//     case 'monday': // day === 'monday'
+//         console.log(`Plan course structure`);
+//         console.log(`Go to coding meetup`);
+//         break;
+//     case 'tuesday':
+//         console.log(`Prepare theory videos`);
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log(`Write code examples`);
+//         break;
+//     case 'friday':
+//         console.log(`Record videos`);
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log(`Enjoy the weekend`)
+//         break;
+//     default:
+//         console.log(`Not a valid day.`)
+// }
+
+if (day === 'monday') {
+    console.log(`Plan course structure`);
+    console.log(`Go to coding bootcamp`);
+} else if (day === 'tuesday') {
+    console.log(`Prepare theory videos`);
+} else if ((day === 'wednesday') || (day === 'thursday')) {
+    console.log(`Write code examples`);
+} else if (day === 'friday') {
+    console.log(`Record videos`);
+} else if ((day === 'saturday') || (day === 'sunday')) {
+    console.log(`Enjoy the weekend!`);
+} else {
+    console.log(`Not a valid day.`);
+}
+*/
