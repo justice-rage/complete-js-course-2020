abd3aedece976810ed6ec753c420066876e17b42
mr-justice <justice.douglas@hey.com>
2020-12-26 13:31:08 -0500

    Completed assignment
    Functions

--- course-materials/complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignments.js
+++ course-materials/complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignments.js
@@ -1,4 +1,13 @@
+/*
 // Part 2 - Lecture Assignments
 
 // Lecture: Functions
 
+function describeCountry(country, population, capitalCity) {
+    console.log(`${country} has ${population} people and its capital city is ${capitalCity}.`);
+}
+
+const USA = describeCountry(`USA`, 375000000, `Washington DC`);
+const Canada = describeCountry(`Canada`, 45000000, `Ottawa`);
+const Mexico = describeCountry(`Mexico`, 126000000, `Mexico City`);
+*/
