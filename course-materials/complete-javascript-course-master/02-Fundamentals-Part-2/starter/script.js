9d8f27cbdc0d29aa48644bfa73ffd1be95e26c11
mr-justice <justice.douglas@hey.com>
2020-12-28 19:43:47 -0500

    Completed lecture
    Functions Calling Other Functions

--- course-materials/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ course-materials/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -74,3 +74,19 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 console.log(yearsUntilRetirement(1995, `Justice`));
 console.log(yearsUntilRetirement(1979, `Patrick`));
 */
+
+// Lecture: Functions Calling Other Functions
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
