aa86ceda0b780ae8b0bddfbda45443f851040dfe
mr-justice <justice.douglas@hey.com>
2021-01-13 15:58:09 -0500

    Completed lecture
    Implementing the Game Logic

--- course-materials/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ course-materials/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -10,11 +10,39 @@
 // document.querySelector(`.guess`).value = 23;
 // console.log(document.querySelector(`.guess`).value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(`.number`).textContent = secretNumber;
+
 document.querySelector(`.check`).addEventListener(`click`, function () {
   const guess = Number(document.querySelector(`.guess`).value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector(`.message`).textContent = `⛔️ No Number!`;
+  } else if (guess === secretNumber) {
+    document.querySelector(`.message`).textContent = `🎉 Correct Number!`;
+  } else if (guess > secretNumber && guess <= 20) {
+    if (score > 1) {
+      document.querySelector(`.message`).textContent = `Too high!`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.message`).textContent = `You lost the game!`;
+      document.querySelector(`.score`).textContent = 0;
+    }
+  } else if (guess < secretNumber && guess >= 1) {
+    if (score > 1) {
+      document.querySelector(`.message`).textContent = `Too low!`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.message`).textContent = `You lost the game!`;
+      document.querySelector(`.score`).textContent = 0;
+    }
+  } else {
+    document.querySelector(`.message`).textContent = `Guess out of bounds.`;
   }
 });
+
+console.log(secretNumber);
