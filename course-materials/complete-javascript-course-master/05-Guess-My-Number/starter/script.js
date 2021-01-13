947a89266497ac39e598d9b9e38ba6efdf46f738
mr-justice <justice.douglas@hey.com>
2021-01-13 18:16:58 -0500

    Completed Coding Challenge
    Coding Challenge #1

--- course-materials/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ course-materials/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -10,9 +10,8 @@
 // document.querySelector(`.guess`).value = 23;
 // console.log(document.querySelector(`.guess`).value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(`.number`).textContent = secretNumber;
 
 document.querySelector(`.check`).addEventListener(`click`, function () {
   const guess = Number(document.querySelector(`.guess`).value);
@@ -25,6 +24,7 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector(`.message`).textContent = `🎉 Correct Number!`;
+    document.querySelector(`.number`).textContent = secretNumber;
 
     document.querySelector(`body`).style.backgroundColor = `#60b347`;
 
@@ -54,6 +54,18 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
   } else {
     document.querySelector(`.message`).textContent = `Guess out of bounds.`;
   }
+});
+
+document.querySelector(`.again`).addEventListener(`click`, function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  let score = 20;
+
+  document.querySelector(`.score`).textContent = score;
+  document.querySelector(`body`).style.backgroundColor = `#222`;
+  document.querySelector(`.message`).textContent = `Start guessing...`;
+  document.querySelector(`.number`).textContent = `?`;
+  document.querySelector(`.number`).style.width = `15rem`;
+  document.querySelector(`.guess`).value = ``;
 });
 
 console.log(secretNumber);
