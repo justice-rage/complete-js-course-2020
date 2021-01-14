2c29aaf103e609e1dd1ff5fe295b8e3e5d743a75
mr-justice <justice.douglas@hey.com>
2021-01-13 19:25:20 -0500

    Completed lecture
    Implementing Highscores

--- course-materials/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ course-materials/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -12,6 +12,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector(`.check`).addEventListener(`click`, function () {
   const guess = Number(document.querySelector(`.guess`).value);
@@ -27,8 +28,12 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
     document.querySelector(`.number`).textContent = secretNumber;
 
     document.querySelector(`body`).style.backgroundColor = `#60b347`;
-
     document.querySelector(`.number`).style.width = `30rem`;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(`.highscore`).textContent = highscore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber && guess <= 20) {
@@ -57,15 +62,15 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
 });
 
 document.querySelector(`.again`).addEventListener(`click`, function () {
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
   let score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector(`.score`).textContent = score;
-  document.querySelector(`body`).style.backgroundColor = `#222`;
   document.querySelector(`.message`).textContent = `Start guessing...`;
+  document.querySelector(`.score`).textContent = score;
   document.querySelector(`.number`).textContent = `?`;
-  document.querySelector(`.number`).style.width = `15rem`;
   document.querySelector(`.guess`).value = ``;
+  document.querySelector(`body`).style.backgroundColor = `#222`;
+  document.querySelector(`.number`).style.width = `15rem`;
 });
 
-console.log(secretNumber);
+console.log(score);
