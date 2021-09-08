02dae58c2581bd1d6bf92fa8f0b07b3c372ec8d3
Justice <64440410+mr-justice@users.noreply.github.com>
2021-09-08 16:53:42 -0700

    Update script.js

--- course-materials/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ course-materials/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -93,5 +93,3 @@ document.querySelector(`.again`).addEventListener(`click`, function () {
   document.querySelector(`body`).style.backgroundColor = `#222`;
   document.querySelector(`.number`).style.width = `15rem`;
 });
-
-c;
